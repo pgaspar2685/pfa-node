88863571ed6887b222b6507ad89895de37b6af2c
Pedro Gaspar <pedrogaspar2685@outlook.com>
2021-04-27 03:40:31 +0100

    container mysql

--- index.js
+++ index.js
@@ -2,31 +2,31 @@ const { response } = require('express')
 const express = require('express')
 const app = express()
 
-
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-    host     : '172.18.0.3',
+    host     : 'db',
     user     : 'pfa-user',
     password : 'pfa-pass',
     database : 'pfa-db'
 });
 
 app.get('/', (req, res) => {
-    res.send("PFA Docker!");
+    res.send("PFA Docker!!!");
 
     con.connect(function(err) {
         if (err) throw err;
         
         console.log("connect");
 
-        let sql = "SELECT * FROM fc-modules";
+        let sql = "SELECT * FROM `fc-modules`";
         con.query(sql, function (err, result) {
             if (err) throw err;
 
             res.send("Result: " + result);
         });
     });
+
 })
 
 app.listen(3000, () => {
